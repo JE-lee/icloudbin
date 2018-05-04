5df186a65c735f34c7c65fb581c9b20501e5e486
lee <1245183029@qq.com>
2018-05-04 15:35:46 +0800

    路径规划

---
+++ utils/config.js
@@ -0,0 +1,3 @@
+export default {
+  GUIDE_KEY: '5dcee99fdbfa152d388eb721f8d9d9a0', // 高德地图开发者key
+}
---
+++ utils/map.js
@@ -0,0 +1,63 @@
+let Guide = require('../lib/guide/amap-wx')
+import config from './config'
+/**
+ * 
+ * @param {String} origin | "longitude,latitude"
+ * @param {String} destination | "longitude,latitude"
+ */
+export function getGuidePath(origin, destination) {
+  return new Promise((resolve, reject) => {
+    wx.showLoading({
+      title: '正在规划路径中......',
+      mask: true
+    })
+    var myAmapFun = new Guide.AMapWX({ key: config.GUIDE_KEY });
+    myAmapFun.getWalkingRoute({
+      origin,
+      destination,
+      success: function (data) {
+        var points = [];
+        if (data.paths && data.paths[0] && data.paths[0].steps) {
+          var steps = data.paths[0].steps;
+          for (var i = 0; i < steps.length; i++) {
+            var poLen = steps[i].polyline.split(';');
+            for (var j = 0; j < poLen.length; j++) {
+              points.push({
+                longitude: parseFloat(poLen[j].split(',')[0]),
+                latitude: parseFloat(poLen[j].split(',')[1])
+              })
+            }
+          }
+        }
+        resolve({
+          points,
+          distance: (data.paths[0] && data.paths[0].distance) ? data.paths[0].distance : -1,
+          cost: (data.paths[0] && data.paths[0].duration) ? parseInt(data.paths[0].duration / 60) : -1
+        })
+        wx.hideLoading()
+
+      },
+      fail: function (info) {
+        reject(`get guide path fail: ${info}`)
+        wx.hideLoading()
+      },
+     
+    })
+  })
+
+}
+
+export function getCurrentPosition() {
+  return new Promise((resolve, reject) => {
+    wx.getLocation({
+      type: 'gcj02', // 返回可直接使用腾讯坐标体系的经纬度
+      success: function ({ longitude, latitude }) {
+        resolve({ longitude, latitude })
+      },
+      fail: function (err) {
+        reject(`get current position fail: ${err}`)
+      }
+    })
+  })
+
+}
